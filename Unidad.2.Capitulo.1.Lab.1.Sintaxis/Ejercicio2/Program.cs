6a49a1212f92f45e1bbb544dfea682a606e2d33b
Fede <federicoclementealvarez@gmail.com>
2023-04-13 02:49:22 -0300

    Ejercicio4 and Ejercicio2 added

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab01
+{
+        internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            string[] arreglo = new string[cantIteraciones];
+
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                Console.WriteLine("Ingrese el elemento número "+i+":");
+                arreglo[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            for (int i = (cantIteraciones-1); i >= 0; i--)
+            {
+                Console.WriteLine("Elemento número " + i + ": " + arreglo[i]);
+            }
+        }
+    }
+}
