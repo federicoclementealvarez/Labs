ce2ba4bd52d66a8515826d49f3bb8c3603ff257a
federico <federicoclementealvarez@gmail.com>
2023-04-24 23:25:59 -0300

    Ejercicio1 and Ejercicio2 added

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Ejercicio1/Ej1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un texto:");
+        string inputTexto = Console.ReadLine();
+        if (inputTexto =="" || inputTexto == null)
+        {
+            Console.WriteLine("Texto no ingresado. Finalizando...");
+        }
+        else
+        {
+            Boolean flag = false;
+            do {
+                ConsoleKeyInfo opcion = election();
+                Console.WriteLine();
+                Console.WriteLine();
+                if (opcion.Key== ConsoleKey.D1)
+                {
+                    Console.WriteLine(inputTexto.ToUpper());
+                }
+                else if (opcion.Key== ConsoleKey.D2)
+                {
+                    Console.WriteLine(inputTexto.ToLower());
+                }
+                else if (opcion.Key== ConsoleKey.D3)
+                {
+                    Console.WriteLine("Longitud: "+inputTexto.Length);
+                }
+                else
+                {
+                    Console.WriteLine("Finalizando...");
+                    flag= true;
+                }
+            } while (flag==false);
+        }
+        Console.ReadKey();
+    }
+
+    public static ConsoleKeyInfo election()
+    {
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Menú:");
+        Console.WriteLine("   (1) - Mayúsculas");
+        Console.WriteLine("   (2) - Minúsculas");
+        Console.WriteLine("   (3) - Cantidad de caracteres");
+        Console.WriteLine("   (0) - Salir");
+        Console.WriteLine("Elija una opción:");
+        ConsoleKeyInfo opcion = Console.ReadKey();
+        return (opcion);
+    }
+}
---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Ejercicio2/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un texto:");
+        string inputTexto = Console.ReadLine();
+        if (inputTexto == "" || inputTexto == null)
+        {
+            Console.WriteLine("Texto no ingresado. Finalizando...");
+        }
+        else
+        {
+            Boolean flag = false;
+            do
+            {
+                ConsoleKeyInfo opcion = election();
+                Console.WriteLine();
+                Console.WriteLine();
+                switch (opcion.Key){
+                    case (ConsoleKey.D1):
+                        Console.WriteLine(inputTexto.ToUpper());
+                        break;
+                    case (ConsoleKey.D2):
+                        Console.WriteLine(inputTexto.ToLower());
+                        break;
+                    case (ConsoleKey.D3):
+                        Console.WriteLine("Longitud: " + inputTexto.Length);
+                        break;
+                    default:
+                        Console.WriteLine("Finalizando...");
+                        flag = true;
+                        break;
+            }
+            } while (flag == false);
+        }
+        Console.ReadKey();
+    }
+
+    public static ConsoleKeyInfo election()
+    {
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Menú:");
+        Console.WriteLine("   (1) - Mayúsculas");
+        Console.WriteLine("   (2) - Minúsculas");
+        Console.WriteLine("   (3) - Cantidad de caracteres");
+        Console.WriteLine("   (0) - Salir");
+        Console.WriteLine("Elija una opción:");
+        ConsoleKeyInfo opcion = Console.ReadKey();
+        return (opcion);
+    }
+}
