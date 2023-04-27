88fd63c84013f5535b6c6a0fff993a7415740262
federico <federicoclementealvarez@gmail.com>
2023-04-26 21:59:47 -0300

    Unidad2Capitulo2Ejercicio2 added

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/Ejercicio2/Ej2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Clases;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        B b = new B();
+        A a = new B();
+        //A a2 = new A();
+        a.F();
+        b.F();
+        //a2.F();
+        a.G();
+        b.G();
+        //a2.G();
+
+        Console.ReadKey();
+
+    }
+}
